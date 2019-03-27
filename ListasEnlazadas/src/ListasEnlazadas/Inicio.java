7b4aa0f9def6ec4790507d5ca83981a2371031b1
bmazariegos <bryanestuard3@gmail.com>
2019-03-26 18:40:59 -0600

    Listas enlazadas nuevas operaciones

--- ListasEnlazadas/src/ListasEnlazadas/Inicio.java
+++ ListasEnlazadas/src/ListasEnlazadas/Inicio.java
@@ -1,23 +1,10 @@
 /*
-<<<<<<< HEAD
  * El programa reliza la creacion de una lista y sus controles
  * Bryan Estuardo Mazariegos Davila 0901-17-1001 
  */
 package ListasEnlazadas;
 
 
-=======
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ListasEnlazadas;
-
-/**
- *
- * @author EDUARDO
- */
->>>>>>> 04b68461570018bcd14dac10482627f48a7198f0
 public class Inicio {
 
     /**
@@ -33,16 +20,13 @@ public class Inicio {
 	miLista.InsertarEnPrimero(60);
 	System.out.println(miLista.Listar());
 	System.out.println("borrar");
-<<<<<<< HEAD
+
         /*miLista.borrarUltimo();
         miLista.borrarPrimero();*/
         System.out.println("Dato buscado: 10\n"+"Dato en la posicion:"+miLista.LocalizarPosicion("10"));
 	System.out.println("Dato a eliminar: 50\n"+miLista.BorrarDato("50"));
-=======
-	miLista.borrarUltimo();
-        miLista.borrarPrimero();
-	
->>>>>>> 04b68461570018bcd14dac10482627f48a7198f0
+
+
 	System.out.println(miLista.Listar());
 // TODO code application logic here
     }
--- ListasEnlazadas/src/ListasEnlazadas/Nodo.java
+++ ListasEnlazadas/src/ListasEnlazadas/Nodo.java
@@ -1,22 +1,10 @@
 /*
-<<<<<<< HEAD
- * esta clase reliza la creacion de un nodo es decir la lista en si
+
+ * Esta clase reliza la creacion de un nodo es decir la lista en si
  * Bryan Estuardo Mazariegos Davila 0901-17-1001 
  */
 package ListasEnlazadas;
 
-=======
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ListasEnlazadas;
-
-/**
- *
- * @author EDUARDO
- */
->>>>>>> 04b68461570018bcd14dac10482627f48a7198f0
 public class Nodo 
 {
     	int info;
